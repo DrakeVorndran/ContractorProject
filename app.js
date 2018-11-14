db28668afcf6f990e166802dbb856e92621cb3af
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-11-14 13:49:46 -0800

    began project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars');
+
+
+
+app.engine('.hbs', exphbs({extname:'.hbs',defaultLayout: 'main'}));
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+  res.render('home', { msg: 'Handlebars are Cool!' });
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
