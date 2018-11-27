b1ccad36e31bf0cca395780f0bb8345aad22854b
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-11-27 11:23:04 -0800

    got donation client names working, continued to add css

--- controllers/donations.js
+++ controllers/donations.js
@@ -1,31 +1,31 @@
 module.exports = function(app) {
     const Client = require('../models/Client');
     const Donation = require('../models/Donation');
-    
-    
+
+
     app.get('/donations', (req,res) => {
         Donation.find().then((donations) => {
             res.render('donations',{donations: donations})
         })
     })
-    
+
     app.post('/donations',(req,res) => {
         Donation.create(req.body).then((donation) => {
-            Client.find({_id: donation.client}).then((client) => {
-                donation.clientName = client.name;
+            Client.find({_id: donation.client}).then((clients) => {
+                donation.clientName = clients[0].name;
+
                 donation.save();
-            console.log(donation);
-                
+
             })
             res.redirect(`/charities/${donation.charity}`)
         })
     })
-    
-    
+
+
     app.delete('/donations/:id',(req,res) => {
         Donation.findByIdAndRemove(req.params.id).then((donation) => {
             console.log("delete donation");
-            res.redirect(`/..`)
+            res.redirect(`/donations`)
         }).catch((err) => {
             console.log(err);
         })
