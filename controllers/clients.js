dc3b244ff9566b74913f8e26ac942ac4fc5a4bac
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-12-12 13:58:49 -0800

    added client edit route

--- controllers/clients.js
+++ controllers/clients.js
@@ -26,6 +26,15 @@ module.exports = function(app) {
         })
     })
 
+    app.get('/clients/:id/edit',(req,res) => {
+        Client.findById(req.params.id).then((client) => {
+            Donation.find({client: req.params.id}).then((donations) => {
+                res.render('clients-edit',{client: client})
+
+            })
+        })
+    })
+
     app.post('/clients', (req, res) => {
         console.log(req.body);
         Client.create(req.body).then(client => {
@@ -37,6 +46,24 @@ module.exports = function(app) {
         })
     })
 
+
+    app.put('/clients/:id', (req,res) => {
+        Client.findByIdAndUpdate(req.params.id, req.body)
+            .then(client => {
+            console.log(req.body);
+            Donation.find({client: client._id}).then((donations) => {
+                console.log(client)
+                for(let i = 0; i<donations.length; i++){
+                    donations[i].clientName = req.body.name;
+                    donations[i].save();
+                }
+            })
+            res.redirect(`/clients/${client._id}`)
+        })
+            .catch(err => {
+            console.log(err.message);
+        })
+    });
 
     app.delete('/clients/:id',(req,res) => {
         Client.findByIdAndRemove(req.params.id).then((client) => {
